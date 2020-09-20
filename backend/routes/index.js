b8345e72f779c5276004e8341d220242d846dd08
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2020-09-20 17:52:36 +0700

    dashboard stories

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
+
+const Story = require('../models/Story')
+
 // @desc  Login/landing page
 // @route GET /
-router.get('/', ensureGuest, function (req, res, next) {
-  // res.render('login', {
-  //   layout: 'login',
-  // });
-    res.send('login');
+router.get('/', (req, res, next) => {
+  res.render('login', {
+    layout: 'login',
+  });
 });
 
 // @desc  dashboard page
 // @route GET /
-router.get('/dashboard',ensureAuth, function (req, res, next) {
-  res.render('dashboard');
+router.get('/dashboard', async (req, res, next) => {
+  try {
+    const stories = await Story.find({user: req.user.id}).lean()
+    res.render('dashboard', {
+    name: req.user.firstName,
+    stories
+    });
+  } catch (error) {
+    console.error(error)
+    res.render('error/500')
+  }
 });
 
 module.exports = router;
