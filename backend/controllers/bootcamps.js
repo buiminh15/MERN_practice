d8ceca21a64ca566803a605748c67b84b0516d44
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2020-09-22 21:58:02 +0700

    update delete course

--- backend/controllers/bootcamps.js
+++ backend/controllers/bootcamps.js
@@ -18,7 +18,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   queryStr = queryStr.replace(/\b(gt|get|lt|lte|in)\b/g, match => `$${match}`)
 
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr))
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses')
 
   // Select Fields
   if (req.query.select) {
--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,12 +1,11 @@
 var express = require('express');
 const {
-  getCourses
+  getCourses, addCourse, getCourse
 } = require('../controllers/courses');
 var router = express.Router({ mergeParams: true });
 
 
-router.route('/').get(getCourses);
-
-
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
