350f32151971353d89770510e520408259fd77e8
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-09-28 08:20:39 +0700

    reset password doing

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -128,30 +128,31 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
  
   await user.save({ validateBeforeSave: false });
 
-  // // Create reset url
-  // const resetUrl = `${req.protocol}://${req.get(
-  //   'host'
-  // )}/api/v1/auth/resetpassword/${resetToken}`;
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/resetpassword/${resetToken}`;
 
-  // const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+ 
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+    
+    res.status(200).json({ success: true, data: 'Email sent' });
+    return;
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
-  // try {
-  //   await sendEmail({
-  //     email: user.email,
-  //     subject: 'Password reset token',
-  //     message,
-  //   });
+    await user.save({ validateBeforeSave: false });
 
-  //   res.status(200).json({ success: true, data: 'Email sent' });
-  // } catch (err) {
-  //   console.log(err);
-  //   user.resetPasswordToken = undefined;
-  //   user.resetPasswordExpire = undefined;
-
-  //   await user.save({ validateBeforeSave: false });
-
-  //   return next(new ErrorResponse('Email could not be sent', 500));
-  // }
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 
   res.status(200).json({
     success: true,
