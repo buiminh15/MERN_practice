61e0ecb346b1feafdc59ae9fb01c12e66fdc9045
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-10-01 17:13:26 +0700

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Bootcamps from './components/bootcamps/Bootcamps';
 import Index from './components/index/Index';
 import Login from './components/login/Login';
@@ -10,10 +10,10 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={Index} />
-          <Route  path={'/login'} component={Login} />
-          <Route  path={'/register'} component={Register} />
-          <Route  path={'/bootcamps'} component={Bootcamps} />
+          <Route exact path={"/"} component={Index} />
+          <Route path={'/login'} component={Login} />
+          <Route path={'/register'} component={Register} />
+          <Route path={'/bootcamps'} component={Bootcamps} />
         </Switch>
       </div>
     </Router>
