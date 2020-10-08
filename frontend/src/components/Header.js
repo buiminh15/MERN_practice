7d25f964621063c1d46ab68e0fd6b980eec1378a
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-10-08 09:32:44 +0700

    done

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from '../img/logo.png';
+export default function Header() {
+    return (
+        <header className="center">
+            <img src={logo} alt="logo"/>
+        </header>
+    )
+}
