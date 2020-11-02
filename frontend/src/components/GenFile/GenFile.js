9f8460af7b55360695887bee6ef44a305b012f0b
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-11-02 18:42:39 +0700

    test

--- frontend/src/components/GenFile/GenFile.js
+++ frontend/src/components/GenFile/GenFile.js
@@ -1,56 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import InputValue from './InputValue';
 import NavbarCustom from './NavbarCustom';
 import { List, ListItem } from '@material-ui/core';
 
 export default function GenFile() {
-  const [data, setData] = React.useState([]);
+    const blankCheck = { name: '', condition_name: [], condition_value: '', result: '' }
+    const [checkState, setCheckState] = useState([{ ...blankCheck }])
 
-  const addComponent = () => {
-    var dataCopy = [...data];
-    dataCopy.push(
-        <ListItem >
-            <InputValue key={dataCopy.length + 1} index={dataCopy.length + 1} />
-        </ListItem>
-    );
-    setData(dataCopy);
-  };
-
-  const getDataHandler = () => {
-    var obj = {
-      input: {},
-    };
-    for (var i = 0; i < data.length; i++) {
-      var key = eval('key' + (i + 1)).value;
-      var val = eval('value' + (i + 1)).value;
-      obj.input[key] = val;
+    const addCheck = () => {
+        setCheckState([...checkState, { ...blankCheck }])
     }
-    console.log(JSON.stringify(obj, null, 4));
-  };
-  return (
-    <>
-      <NavbarCustom />
-      <div className="container" style={{ marginTop: '80px' }}>
-        <List>{data}</List>
-        <div className="row">
-          <div className="col col-4"></div>
-          <div className="col col-4">
-            <Button variant="contained" color="primary" onClick={addComponent}>
-              Add More
+
+    const handleCheckChange = (e) => {
+        const updatedChecks = [...checkState]
+        updatedChecks[e.target.dataset.idx][e.target.className] = e.target.value;
+        setCatState(updatedChecks);
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`22222 `,ownerState );
+        console.log(`1111 `,catState );
+      };
+    const [data, setData] = React.useState([]);
+
+    const addComponent = () => {
+        var dataCopy = [...data];
+        dataCopy.push(
+            <ListItem >
+                <InputValue key={dataCopy.length + 1} index={dataCopy.length + 1} />
+            </ListItem>
+        );
+        setData(dataCopy);
+    };
+
+    const getDataHandler = () => {
+        var obj = {
+            input: {},
+        };
+        for (var i = 0; i < data.length; i++) {
+            var key = eval('key' + (i + 1)).value;
+            var val = eval('value' + (i + 1)).value;
+            obj.input[key] = val;
+        }
+        console.log(JSON.stringify(obj, null, 4));
+    };
+    return (
+        <>
+            <NavbarCustom />
+            <div className="container" style={{ marginTop: '80px' }}>
+                <List>{data}</List>
+                <div className="row">
+                    <div className="col col-4"></div>
+                    <div className="col col-4">
+                        <Button variant="contained" color="primary" onClick={addComponent}>
+                            Add More
             </Button>
-          </div>
-          <div className="col col-4">
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={getDataHandler}
-            >
-              Gen Data
+                    </div>
+                    <div className="col col-4">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={getDataHandler}
+                        >
+                            Gen Data
             </Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
--- frontend/src/components/GenFile/InputValue.js
+++ frontend/src/components/GenFile/InputValue.js
@@ -1,125 +1,71 @@
 import React, { useEffect } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import {
-  FormGroup,
-  InputLabel,
   MenuItem,
-  FormControl,
-  Select,
-  NativeSelect,
   InputBase,
   TextField,
   Button,
 } from '@material-ui/core';
 
-const BootstrapInput = withStyles((theme) => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing(3),
-    },
-  },
-  input: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-}))(InputBase);
-
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-}));
-// const inputs = [
-//   {
-//     name: 'input',
-//     values: ['max-length', 'min-length'],
-//     index: 1,
-//   },
-//   {
-//     name: 'checkbox',
-//     values: ['checked', 'uncheck'],
-//     index: 2,
-//   },
-// ];
 const inputs = [
   {
     name: 'input',
-    values_name: ['max-length'],
+    condition_name: ['max-length', 'min-length'],
+    condition_value: '2',
+    result: 'aaaaa',
     index: 1,
+
   },
   {
     name: 'checkbox',
-    values_name: ['checked'],
+    values_name: ['checked', 'uncheck'],
     index: 2,
   },
 ];
-export default function InputValue(props) {
-  const classes = useStyles();
+
+export default function InputValue({ idx, checkState, handleCheckChange, handleChange, handleConditionChange }) {
+  const field1Id = `field1-${idx}`
+  const field2Id = `field2-${idx}`
+  const field3Id = `field3-${idx}`
+  const field4Id = `field4-${idx}`
+  const field5Id = `field5-${idx}`
+
   const [input, setInput] = React.useState('');
   const [condition, setCondition] = React.useState('');
   const [disabled, setDisabled] = React.useState(true);
   const valuesArr = [];
-  useEffect(() => {
-    console.log('hello', input);
-    inputs.find((obj) => {
-      if (obj.name === input) {
-        obj.values_name.map((val) =>
-          valuesArr.push(
-            <MenuItem key={val} value={val}>
-              {val}
-            </MenuItem>
-          )
-        );
-      }
-    });
-  }, [input]);
-  useEffect(() => {
-    function handleConditionChangeA() {
-        setCondition('input');
-    }
-    return () => {handleConditionChangeA()};
-  }, [condition]);
+  // useEffect(() => {
+  //   console.log('hello', input);
+  //   inputs.find((obj) => {
+  //     if (obj.name === input) {
+  //       obj.values_name.map((val) =>
+  //         valuesArr.push(
+  //           <MenuItem key={val} value={val}>
+  //             {val}
+  //           </MenuItem>
+  //         )
+  //       );
+  //     }
+  //   });
+  // }, [input]);
 
-  const handleChange = (event) => {
-    setInput(event.target.value);
-    setDisabled(false)
-  };
+  // const handleChange = (event) => {
+  //   setInput(event.target.value);
+  //   setDisabled(false)
+  // };
 
-  const handleConditionChange = (event) => {
-    console.log('aaaa ', event.target.value);
-    setCondition(event.target.value);
-  };
+  // const handleConditionChange = (event) => {
+  //   setCondition(event.target.value);
+  // };
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
+    <div key={`field1-${idx}`}>
       <TextField
-        id="outlined-select-currency"
+        id={field1Id}
+        name={field1Id}
         select
         label="Select"
-        value={input}
+        value={checkState[idx].name}
         onChange={handleChange}
         helperText="Please select your inputs"
         variant="outlined"
@@ -132,11 +78,12 @@ export default function InputValue(props) {
       </TextField>
 
       <TextField
-        id="outlined-select-currency"
+        id={field2Id}
+        name={field2Id}
         disabled={disabled}
         select
-        label="Select Condition"
-        value={condition}
+        label="Select Condition Name"
+        value={checkState[idx].condition_name}
         onChange={handleConditionChange}
         helperText="Please select your condition"
         variant="outlined"
@@ -145,89 +92,30 @@ export default function InputValue(props) {
       </TextField>
 
       <TextField
-        id="outlined-helperText"
-        label="Enter Value"
+        id={field3Id}
+        name={field3Id}
+        label="Enter Condition Value"
+        value={checkState[idx].condition_value}
         defaultValue="Default Value"
         helperText="Some important text"
         variant="outlined"
+        onChange={handleCheckChange}
       />
 
       <TextField
-        id="outlined-textarea"
+        id={field4Id}
+        name={field4Id}
         disabled={disabled}
-        label="Multiline KQ"
+        value={checkState[idx].result}
+        label="Result"
         placeholder="Placeholder"
         multiline
         variant="outlined"
+        onChange={handleCheckChange}
       />
       <Button variant="outlined" color="secondary">
         X
       </Button>
-    </form>
-
-    // <FormGroup>
-    //   <div className="row">
-    //     <div className="col col-2">
-    //       <FormControl className={classes.margin}>
-    //         <InputLabel htmlFor="demo-customized-textbox">Type </InputLabel>
-    //         <Select
-    //           labelId="demo-customized-select-label"
-    //           style={{ width: 'auto' }}
-    //           value={items}
-    //           placeholder="assf"
-    //           input={<BootstrapInput id={'key' + props.index} />}
-    //         >
-    //           {datas &&
-    //             datas.map((item, index) => (
-    //               <MenuItem
-    //                 value={item}
-    //                 onClick={() => handleChange(item, index)}
-    //               >
-    //                 {item}
-    //               </MenuItem>
-    //             ))}
-    //         </Select>
-    //       </FormControl>
-    //     </div>
-
-    //     <div className="col col-3">
-    //       <FormControl className={classes.margin}>
-    //         <InputLabel htmlFor="demo-customized-textbox">Key</InputLabel>
-    //         <Select
-    //           labelId="demo-customized-select-label"
-    //           style={{ width: '150px' }}
-    //           value={items}
-    //           input={<BootstrapInput id={'key' + props.index} />}
-    //         >
-    //           {datas &&
-    //             datas.map((item, index) => (
-    //               <MenuItem
-    //                 value={item}
-    //                 onClick={() => handleChange(item, index)}
-    //               >
-    //                 {item}
-    //               </MenuItem>
-    //             ))}
-    //         </Select>
-    //       </FormControl>
-    //     </div>
-    //     <div className="col col-3">
-    //       <FormControl className={classes.margin}>
-    //         <InputLabel htmlFor="demo-customized-textbox">Value</InputLabel>
-    //         <BootstrapInput id={'value' + props.index} />
-    //       </FormControl>
-    //     </div>
-
-    //     <div className="col col-3">
-    //       <FormControl className={classes.margin}>
-    //         <InputLabel htmlFor="demo-customized-textbox">Result</InputLabel>
-    //         <BootstrapInput id={'value' + props.index} />
-    //       </FormControl>
-    //     </div>
-    //     <div className="col col-2">
-    //         <button className="btn btn-danger">X</button>
-    //     </div>
-    //   </div>
-    // </FormGroup>
+    </div>
   );
 }
