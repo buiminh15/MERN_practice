7c05835727168c65a5627ac1a174c42b8effb265
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-12-18 19:49:55 +0700

    add card

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react'
 import Product from './Product.js';
 import Title from './Title'
 import { storeProducts } from '../data'
+import { ProductConsumer } from '../context.js';
 export default class ProductList extends Component {
     state = {
         products: storeProducts
     }
     render() {
-        console.log(this.state.products);
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                         <Title name="our" title="products" />
                         <div className="row">
-
+                            <ProductConsumer>
+                                {value => {
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product} handleDetail={value.handleDetail} />
+                                    })
+                                }}
+                            </ProductConsumer>
                         </div>
                     </div>
                 </div>
