8de895b818f77c58cbd10306955bd84bea30f054
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-12-19 19:13:27 +0700

    doing modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
@@ -8,6 +7,8 @@ import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
+import Modal from './components/Modal';
+
 function App() {
   return (
     <React.Fragment>
@@ -18,6 +19,7 @@ function App() {
         <Route path='/cart' component={Cart} />
         <Route component={Default} />
       </Switch>
+      <Modal />
     </React.Fragment>
   );
 }
---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { ProductConsumer } from '../context'
+import { ButtonContainer } from './Button';
+import { Link } from 'react-router-dom'
+
+
+export default class Modal extends Component {
+    render() {
+        return (
+            <h1>
+                hello from modal
+            </h1>
+        )
+    }
+}
