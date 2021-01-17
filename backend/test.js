8a2e44af9945ab63caeb169e24457f69899b3c94
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2021-01-17 18:53:45 +0700

    ass

--- backend/test.js
+++ backend/test.js
@@ -1,8 +1,17 @@
-const generator = require('test-file-generator/lib/classes/TestFileGenerator');
-console.time('start')
-let genFile = new generator.TestFileGenerator('txt');
-genFile.setSize(2000)
-genFile.generateFile()
-console.timeEnd('start');
+// const generator = require('test-file-generator/lib/classes/TestFileGenerator');
+// console.time('start')
+// let genFile = new generator.TestFileGenerator('txt');
+// genFile.setSize(2000)
+// genFile.generateFile()
+// console.timeEnd('start');
 // console.log(new generator.TestFileGenerator('gif').generateFile());
 
+const translate = require('translate-google');
+
+translate('I speak Chinese', { to: 'zh-cn' })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
