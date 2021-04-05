e8a2d7fa91dac1691380e0c90674030943413fc1
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2021-04-05 11:28:46 +0700

    add

--- frontend/src/views/components/card/TesterCardList.js
+++ frontend/src/views/components/card/TesterCardList.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { features } from '../../../models/features'
+import Card from './Card'
 
 export default function TesterCardList() {
     return (
         <div>
-            Link 
-            Description
+            {features.map((feature) => (
+                <div className="col-4 my-3" key={feature.name}>
+                    <Card key={feature.name} {...feature} />
+                </div>
+            ))}
         </div>
     )
 }
